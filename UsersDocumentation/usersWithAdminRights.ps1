644511f8cb74c51f133e3afa3054f7ad96221791
SzymonSt <szymonst2808@gmail.com>
2021-05-14 14:40:20 +0200

    localGroup fix

--- UsersDocumentation/usersWithAdminRights.ps1
+++ UsersDocumentation/usersWithAdminRights.ps1
@@ -1,6 +1,11 @@
-Get-LocalGroup;
+$gs = Get-LocalGroup | Out-String;
+Write-Host $gs;
 $g = Read-Host -Prompt "Choose Group Of Users with specific rights"
 $AdminRights = Get-LocalGroupMember -Group $g;
 $users = $AdminRights | Where-Object -Property ObjectClass -EQ "User";
 $groups = $AdminRights | Where-Object -Property ObjectClass -EQ "Group";
-$users.Name | ConvertTo-Json;
+$netConfDta = [PSCustomObject]@{
+   Users = $users.Name;
+   Groups = $groups.Name;
+}
+$netConfDta | ConvertTo-Json;
