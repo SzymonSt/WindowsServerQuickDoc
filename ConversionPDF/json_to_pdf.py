8b7710c797491327b924198a8354ae0264fc3112
SzymonSt <szymonst2808@gmail.com>
2021-05-16 18:52:17 +0200

    Update json_to_pdf.py

--- ConversionPDF/json_to_pdf.py
+++ ConversionPDF/json_to_pdf.py
@@ -1,6 +1,11 @@
 import json
 file = open("C:\\Users\\lenovo\\Documents\\GitHub\\WindowsServerQuickDoc\\NetConfigDocumentation\\dummy\\net_conf.json")
 data = json.load(file)
-htmlString = '<head><style>table, th, td {border: 1px solid black;}</style></head><table><tr><td>Ip</td><td>'+str(data["Ip"])+'</td></tr><tr><td>Subnet Mask</td><td>'+str(data["SubnetMask"])+'</td></tr></table>'
+htmlStrings = []
+# htmlString = '<head><style>table, th, td {border: 1px solid black;}</style></head><table><tr><td>Computer Name</td><td>'+str(data["ComputerName"])+'</td></tr><tr><td>Domain</td><td>'+str(data["Domain"])+'</td></tr><tr><td>Interface Name</td><td>'+str(data["InterfaceName"])+'</td></tr><tr><td>Ip</td><td>'+str(data["Ip"])+'</td></tr><tr><td>Subnet Mask</td><td>'+str(data["SubnetMask"])+'</td></tr></table>'
+htmlBeggining = '<head><style>table, th, td {border: 1px solid black;}</style></head><table>'
+htmlEnding = '</table>'
+for dta in data:
+    tmpStr  = '<tr><td>'+dta+'</td><td>'+str(dta)+'</td></tr>'
 f = open("test.html","w")
 f.write(htmlString)
