8507800f4148e27a620d8be9d66dd5ed332aba4e
SzymonSt <szymonst2808@gmail.com>
2021-05-16 19:54:58 +0200

    html conversion

--- ConversionHTML/json_to_pdf.py
+++ ConversionHTML/json_to_pdf.py
@@ -2,7 +2,6 @@ import json
 file = open("C:\\Users\\lenovo\\Documents\\GitHub\\WindowsServerQuickDoc\\UsersDocumentation\\dummy\\users_conf.json")
 data = json.load(file)
 htmlStrings = []
-# htmlString = '<head><style>table, th, td {border: 1px solid black;}</style></head><table><tr><td>Computer Name</td><td>'+str(data["ComputerName"])+'</td></tr><tr><td>Domain</td><td>'+str(data["Domain"])+'</td></tr><tr><td>Interface Name</td><td>'+str(data["InterfaceName"])+'</td></tr><tr><td>Ip</td><td>'+str(data["Ip"])+'</td></tr><tr><td>Subnet Mask</td><td>'+str(data["SubnetMask"])+'</td></tr></table>'
 htmlBeggining = '<head><style>table, th, td {border: 1px solid black;}</style></head><table>'
 htmlEnding = '</table>'
 for dta in data:
