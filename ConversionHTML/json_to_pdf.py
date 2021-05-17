9434bb2d28c5024b4e218ebaf99b7e274588624e
SzymonSt <szymonst2808@gmail.com>
2021-05-17 21:10:18 +0200

    html beautifuler

--- ConversionHTML/json_to_pdf.py
+++ ConversionHTML/json_to_pdf.py
@@ -2,8 +2,9 @@ import json
 file = input("Provide input file path: ")
 file = open(file)
 fileOut = input("Provide output file path(if file is existing will be appended): ")
+titleOfDoc = input("Provide title of your doc: ")
 data = json.load(file)
-htmlBeggining = '<head><style>table, th, td {border: 1px solid black;}</style></head><table>'
+htmlBeggining = '<head><style>table, th, td {border: 1px solid black;} h1{text-align:center;}</style></head><h1>'+titleOfDoc+'</h1><table>'
 htmlEnding = '</table>'
 for dta in data:
     tmpStr  = '<tr><td>'+dta+'</td><td>'+str(data[dta])+'</td></tr>'
